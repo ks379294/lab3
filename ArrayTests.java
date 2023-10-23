e767504bdd8385feaf5cfb5e0fa32c8aedde0638
kendras5909 <kendras5909@gmail.com>
2023-10-23 16:39:02 -0700

    fixed bugs

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,6 +1,6 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-
+import static java.lang.System.*;
 public class ArrayTests {
 	@Test 
 	public void testReverseInPlace() {
@@ -8,11 +8,27 @@ public class ArrayTests {
     ArrayExamples.reverseInPlace(input1);
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
-
+  @Test 
+	public void testReverseInPlace_twoElement() {
+    int[] input1 = { 2, 3, 5, 6,7 };
+    ArrayExamples.reverseInPlace(input1);
+    
+    for(int i = 0; i<input1.length; i++)
+    {
+       System.out.println(i + ": "+ input1[i]);
+    }
+     assertArrayEquals(new int[]{7, 6, 5, 3, 2}, input1);
+	}
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
   }
+  @Test
+  public void testReversed_nonEmpty() {
+    int[] input1 = {15, 16, 17};
+    assertArrayEquals(new int[]{17, 16, 15}, ArrayExamples.reversed(input1));
+  }
 }
+
